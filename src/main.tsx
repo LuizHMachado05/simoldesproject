e00a3315bcf3f4ee371756023b9a93ade79a08cb
130520034Lucas <lucassimioni13@yahoo.com>
2025-05-20 20:16:39 -0300

    testando

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+console.log('Iniciando aplicação, BASE_URL:', import.meta.env.BASE_URL);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error('Elemento root não encontrado!');
+}
