3c491613d8c3bebbb1444be94255677ea3a932cd
Luiz Henrique da silva Machado <machadoluiz659@gmail.com>
2025-07-01 16:37:45 -0300

    vercel2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/simoldesproject/',  // Nome correto do repositório
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
